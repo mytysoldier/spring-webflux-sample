32bb75c2d843c79fe148d71dd471b7a7a5357da0
mytysoldier <yoshi008t@gmail.com>
2021-10-03 15:24:13 +0900

    READMEの編集、サンプルコードを更新

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # spring-webflux-sample
+以下両パターンの検証
+- アノテーションベースでの実装（HelloController.java）
+- 関数型プログラミングでの実装（HelloFunction.java）
+
+## メモ（ざっくり調べた内容なので、もしかしたら間違っているものもあるかもしれません。。）
+- Spring WebFluxのデフォルトアプリケーションサーバーはNettyである
+- 戻り値にMonoとFluxを使える
+- Monoはレスポンスを1つ含められる
+- Fluxはレスポンスを複数含められる
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,9 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/monohello")
+    public Mono<String> monohello() {
+        return Mono.just("monohello");
+    }
 
     @GetMapping("/fluxhello")
     public Flux<String> fluxhello() {
